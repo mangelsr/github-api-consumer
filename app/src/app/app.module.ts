c13f110d238e0c5848c3ed32559bc0eac3b79f6b
Miguel Sanchez <mangelsr25@gmail.com>
2021-06-29 21:55:24 -0500

    [ADD] SearchUser Component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SearchUserComponent } from './pages/search-user/search-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
