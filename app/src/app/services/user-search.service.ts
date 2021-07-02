d6253972aafb0365d27fd2d78a384f33509b6a25
Miguel Sanchez <mangelsr25@gmail.com>
2021-07-02 00:08:00 -0500

    [ADD] Request logic in service

--- app/src/app/services/user-search.service.ts
+++ app/src/app/services/user-search.service.ts
@@ -1,13 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Repository } from '../models/repository.model';
+
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSearchService {
 
-  constructor() { }
+  BASE_URL = 'https://api.github.com';
+  loading = false;
+  error = '';
+  user: User | undefined;
 
-  searchUser(username: String) {
-    console.log(username);
+  constructor(private http: HttpClient) { }
+
+  async searchUser(username: String) {
+    try {
+      this.loading = true;
+      this.user = undefined;
+      this.error = '';
+      this.user = await this.http.get<User>(`${this.BASE_URL}/users/${username}`).toPromise();
+      const repos = await this.http.get<Repository[]>(`${this.user.repos_url}`).toPromise();
+    } catch (err) {
+      this.error = err.error.message;
+    } finally {
+      this.loading = false;
+    }
   }
 }
